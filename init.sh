240f5315810d68a32d1d3d1cc294e7e7acd9b51c
dylan <zran1988@gmail.com>
2017-11-08 18:33:52 +0800

    fix errors

--- init.sh
+++ init.sh
@@ -3,9 +3,19 @@
 # Author: ranzhang@mobvoi.com (Dylan)
 # environment configuration
 
+cd ~/
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/google/vim-glaive.git ~/.vim/bundle/vim-glave
 sudo apt-get install clang-format-3.5
 sudo apt-get install gccgo-go
 
+scp ~/dev-env/.bashrc ~/
+scp ~/dev-env/.vimrc ~/
+source ~/.bashrc
+
 mkdir bin
 ln -fs /usr/bin/clang-format-3.5 bin/clang-format
-mkdir -p Workspace/gocode
+mkdir -p workspace/gocode
+go get github.com/bazelbuild/buildtools/buildifier
+
