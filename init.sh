42366eb564fa2e7d614e25dea55cc07010582822
handsomedylan1988 <handsomedylan1988@users.noreply.github.com>
2018-08-03 10:18:55 +0800

    remove clang-format version and soft link

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ cd ~/
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/google/vim-glaive.git ~/.vim/bundle/vim-glave
-sudo apt-get install clang-format-3.5
+sudo apt-get install clang-format
 sudo apt-get install gccgo-go
 sudo apt-get install js-beautify
 
@@ -16,7 +16,6 @@ scp ~/dev-env/.vimrc ~/
 source ~/.bashrc
 
 mkdir bin
-ln -fs /usr/bin/clang-format-3.5 bin/clang-format
 mkdir -p workspace/gocode
 go get github.com/bazelbuild/buildtools/buildifier
 
